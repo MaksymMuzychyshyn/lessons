4d65019f49cfa7125bb67ef46c417b7a00ad4213
Maks1mMuzychyshyn <muzychyshyn.maksim@gmail.com>
2021-09-23 11:41:21 +0300

    add

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-
-export const getTitle = () => {
+export const setTitle = text => {
     const elementTitle = document.querySelector('.title');
-    return elementTitle.textContent;
-  };
-  
-  export const getDescription = () => {
-    const elemenDescr = document.querySelector('.about');
-    return elemenDescr.innerText;
-  };
-  
-  export const getPlans = () => {
-    const elemenPlans = document.querySelector('.plans');
-    return elemenPlans.innerHTML;
-  };
-  
-  export const getGoal = () => {
-    const elemenGoal = document.querySelector('.goal');
-    return elemenGoal.outerHTML;
+    elementTitle.textContent = text;
   };
