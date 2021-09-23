c522993fd3163d1a7709c6ed9559084c03600588
Maks1mMuzychyshyn <muzychyshyn.maksim@gmail.com>
2021-09-23 11:34:39 +0300

    add

---
+++ index.js
@@ -0,0 +1,16 @@
+   
+export const getTitleElement = () => {
+    const titleElem = document.querySelector('.title');
+    console.dir(titleElem);
+    return titleElem;
+  };
+  
+  getTitleElement();
+  
+  export const getInputElement = () => {
+    const inputElem = document.querySelector('input', '[type = text]');
+    console.dir(inputElem);
+    return inputElem;
+  };
+  
+  getInputElement();
