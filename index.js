c693eecffbec8dccf92c9e9a3f48824bcf3cd757
Maks1mMuzychyshyn <muzychyshyn.maksim@gmail.com>
2021-09-24 08:34:06 +0300

    add

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-export const manageClasses = () => {
-  const elOne = document.querySelector('.one');
-  elOne.classList.add('selected');
-
-  const elTwo = document.querySelector('.two');
-  elTwo.classList.remove('selected');
-
-  const elThree = document.querySelector('.three');
-  elThree.classList.toggle('three_done');
-
-  const elFour = document.querySelector('.four');
-  if (elFour.classList.contains('some-class')) {
-    elFour.classList.add('another-class');
-  }
+export const squaredNumbers = () => {
+  const getNum = document.querySelectorAll('.number');
+  [...getNum].map(el => (el.dataset.squaredNumber = el.dataset.number ** 2));
 };
+squaredNumbers();
