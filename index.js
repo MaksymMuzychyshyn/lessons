605c04e66b70e6642bd5bc1e179225ef1eecd9e9
Maks1mMuzychyshyn <muzychyshyn.maksim@gmail.com>
2021-09-24 08:25:57 +0300

    add

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-
-export const finishList = () => {
-  const elemLi1 = document.createElement('li');
-  const elemLi4 = document.createElement('li');
-  const elemLi6 = document.createElement('li');
-  const elemLi8 = document.createElement('li');
-  elemLi1.textContent = '1';
-  elemLi4.textContent = '4';
-  elemLi6.textContent = '6';
-  elemLi8.textContent = '8';
-  const elemList = document.querySelector('.list');
-  elemList.append(elemLi8);
-  elemList.prepend(elemLi1);
-  const elemSpec = document.querySelector('.special');
-  elemSpec.before(elemLi4);
-  elemSpec.after(elemLi6);
+export const finishForm = () => {
+  const elemList = document.querySelector('input');
+  elemList.setAttribute('type', 'text');
+  elemList.setAttribute('name', 'login');
+  const inputNew = document.createElement('input');
+  elemList.append(inputNew);
+  inputNew.setAttribute('type', 'password');
+  inputNew.setAttribute('name', 'password');
 };
