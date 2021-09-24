f961aee0cc94ba1977261bd9af845160bea2250e
Maks1mMuzychyshyn <muzychyshyn.maksim@gmail.com>
2021-09-24 08:22:46 +0300

    add

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-   
 
-export const createButton = buttonText => {
-  const buttonElem = document.createElement('button');
-  buttonElem.textContent = buttonText;
-  const listElem = document.querySelector('body');
-  listElem.append(buttonElem);
+export const finishList = () => {
+  const elemLi1 = document.createElement('li');
+  const elemLi4 = document.createElement('li');
+  const elemLi6 = document.createElement('li');
+  const elemLi8 = document.createElement('li');
+  elemLi1.textContent = '1';
+  elemLi4.textContent = '4';
+  elemLi6.textContent = '6';
+  elemLi8.textContent = '8';
+  const elemList = document.querySelector('.list');
+  elemList.append(elemLi8);
+  elemList.prepend(elemLi1);
+  const elemSpec = document.querySelector('.special');
+  elemSpec.before(elemLi4);
+  elemSpec.after(elemLi6);
 };
