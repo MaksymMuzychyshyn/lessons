e9788b2be33272554cc9b6af134dd87047b99047
Maks1mMuzychyshyn <muzychyshyn.maksim@gmail.com>
2021-09-24 08:45:19 +0300

    add

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
-export const getSection = num => {
-  const getSpan = document.querySelector(`span[data-number="${num}"]`);
-  const result = getSpan.closest('.box').dataset.section;
-  return result;
+const tasks = [
+  { text: 'Buy milk', done: false },
+  { text: 'Pick up Tom from airport', done: false },
+  { text: 'Visit party', done: false },
+  { text: 'Visit doctor', done: true },
+  { text: 'Buy meat', done: true },
+];
+
+const renderListItems = listItems => {
+  const listElem = document.querySelector('.list');
+
+  const listItemElems = listItems
+    .sort((a, b) => a.done - b.done)
+    .map(({ text, done }) => {
+      const listItemElem = document.createElement('li');
+
+      listItemElem.classList.add('list__item');
+      if (done) {
+        listItemElem.classList.add('list__item_done');
+      }
+      const checkBoxElem = document.createElement('input');
+      checkBoxElem.setAttribute('type', 'checkbox');
+      checkBoxElem.checked = done;
+
+      checkBoxElem.classList.add('list__item-checkbox');
+      listItemElem.append(checkBoxElem, text);
+
+      return listItemElem;
+    });
+
+  listElem.append(...listItemElems);
 };
+
+renderListItems(tasks);
