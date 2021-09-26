6db7b0acf11def518cd2692852705414569b9db7
Maks1mMuzychyshyn <muzychyshyn.maksim@gmail.com>
2021-09-26 20:47:16 +0300

    add

--- index.js
+++ index.js
@@ -1,6 +1,68 @@
 
-const elPage = document.querySelectorAll('.pagination__page');
-const handleClick = event => {
-  console.log(event.target.dataset.pageNumber);
+// // так можно получить данные формы - ВАРИАНТ 1:
+// const formFields = [...new FormData(formElem)];
+// // formFields => [["email", "значение поля email"], ["password", "значение поля password"]]
+
+// const formData = formFields.reduce(function (acc, formField) {
+//   const prop = formField[0]; // здесь "name" инпута
+//   const value = formField[1]; // здесь "value" инпута
+//   // если использовать деструктуризацию, то можно предыдущие 2 строки записать короче
+//   // const [prop, value] = formField;
+//   return {
+//     // используем оператор расширения, чтобы в acc добвить свойства все предыдущих итераций
+//     ...acc,
+//     // используем вычислимое свойство объекта, чтобы добавить в аккумулятор новое свойство
+//     [prop]: value,
+//   };
+// }, {});
+
+// //более простой формат считывания формы - ВАРИАНТ 2:
+// const formData = Object.fromEntries(new FormData(formElem));
+
+const emailInputElem = document.querySelector('#email');
+const passwordlInputElem = document.querySelector('#password');
+
+const emailErrorElem = document.querySelector('.error-text_email');
+const passwordErrorElem = document.querySelector('.error-text_password');
+
+const isRequired = value => (value ? undefined : 'Required');
+const isEmail = value => (value.includes('@') ? undefined : 'Should be an email');
+
+const validatorsByField = { email: [isRequired, isEmail], password: [isRequired] };
+
+const validate = (fieldName, value) => {
+  const validators = validatorsByField[fieldName];
+  return validators
+    .map(validator => validator(value))
+    .filter(errorText => errorText)
+    .join(', ');
 };
-[...elPage].map(el => el.addEventListener('click', handleClick));
+
+const onEmailChange = event => {
+  const errorText = validate('email', event.target.value);
+  emailErrorElem.textContent = errorText;
+};
+
+const onPasswordChange = event => {
+  const errorText = validate('password', event.target.value);
+  passwordErrorElem.textContent = errorText;
+};
+
+emailInputElem.addEventListener('input', onEmailChange);
+passwordlInputElem.addEventListener('input', onPasswordChange);
+
+const formElem = document.querySelector('.login-form');
+
+const onFormSubmit = event => {
+  event.preventDefault();
+  const formData = [...new FormData(formElem)].reduce(
+    (acc, [field, value]) => ({
+      ...acc,
+      [field]: value,
+    }),
+    {},
+  );
+  alert(JSON.stringify(formData));
+};
+
+formElem.addEventListener('submit', onFormSubmit);
