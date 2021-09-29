fad9370f2b64277e6ab42e92f3deaf72dbdd5572
Maks1mMuzychyshyn <muzychyshyn.maksim@gmail.com>
2021-09-29 08:46:14 +0300

    add

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-export const dayOfWeek = (date, days) => {
-  const weekDays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
-  const dayNow = new Date(date).getDay();
-  const dayInFuture = new Date(date).setDate(dayNow + days);
-  return weekDays[new Date(dayInFuture).getDate()];
-};
+export const getGreenwichTime = date => {
+  const formatter = new Intl.DateTimeFormat('en', {
+      timeZone: 'UTC',
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: false
+  });
+  return formatter.format(date);
+}
