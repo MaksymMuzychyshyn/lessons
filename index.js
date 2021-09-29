c0b6929b1d13ee5d48fd5ee9913fa8db1ba968ed
Maks1mMuzychyshyn <muzychyshyn.maksim@gmail.com>
2021-09-29 08:23:37 +0300

    add

--- index.js
+++ index.js
@@ -1,60 +1,18 @@
+   
+const getDiff = (startDate, endDate) => {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-const tasks = [
-  { text: 'Buy milk', done: false, id: '1' },
-  { text: 'Pick up Tom from airport', done: false, id: '2' },
-  { text: 'Visit party', done: false, id: '3' },
-  { text: 'Visit doctor', done: true, id: '4' },
-  { text: 'Buy meat', done: true, id: '5' },
-];
-const listElem = document.querySelector('.list');
-const renderListItems = listItems => {
-  const listItemElems = listItems
-    .sort((a, b) => a.done - b.done)
-    .map(({ text, done, id }) => {
-      const listItemElem = document.createElement('li');
-      listItemElem.classList.add(`list__item`);
-      listItemElem.dataset.id = id;
-      if (done) {
-        listItemElem.classList.add('list__item_done');
-      }
-      const checkBoxElem = document.createElement('input');
-      checkBoxElem.classList.add('list__item-checkbox');
-      checkBoxElem.setAttribute('type', 'checkbox');
-      checkBoxElem.checked = done;
-      listItemElem.append(checkBoxElem, text);
-      return listItemElem;
-    });
-  listElem.innerHTML = '';
-  listElem.append(...listItemElems);
-};
-renderListItems(tasks);
-
-const addTask = event => {
-  const inputElem = document.querySelector('.task-input');
-  if (inputElem.value === '') {
-    return;
-  }
-  const task = {
-    text: inputElem.value,
-    done: false,
-    id: `${tasks.length + 1}`,
-  };
-  tasks.push(task);
-  inputElem.value = '';
-  renderListItems(tasks);
-};
-const createBtn = document.querySelector('.create-task-btn');
-createBtn.addEventListener('click', addTask);
-
-const updateTask = event => {
-  const isCheckbox = event.target.classList.contains('list__item-checkbox');
-  if (!isCheckbox) {
-    return;
-  }
-  const currentItem = event.target.closest('.list__item');
-  const currentTask = tasks.find(item => item.id === currentItem.dataset.id);
-  currentTask.done = event.target.checked;
-  renderListItems(tasks);
+  const difference = Math.abs(endDate - startDate);
+  const daysDiff = Math.floor(difference / day);
+  const hoursDiff = Math.floor((difference % day) / hour);
+  const minutesDiff = Math.floor((difference % hour) / minute);
+  const secondsDiff = Math.floor((difference % minute) / second);
+  return `${daysDiff}d ${hoursDiff}h ${minutesDiff}m ${secondsDiff}s`;
 };
 
-listElem.addEventListener('click', updateTask);
+const start = new Date(2020, 10, 10, 13, 40, 7);
+const end = new Date(2020, 11, 13, 15, 38, 1);
+console.log(getDiff(start, end));
